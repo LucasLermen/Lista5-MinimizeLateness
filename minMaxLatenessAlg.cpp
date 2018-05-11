74678e8a30aa8d7d3bcc8ebb67013e7da3ef7e87
LucasLermen <lucas.arthur.lermen@gmail.com>
2018-05-10 21:03:57 -0300

    Adicionando eventos ordenados na lista.
    Co-authored-by: drianne<driah_as@hotmail.com>

--- minMaxLatenessAlg.cpp
+++ minMaxLatenessAlg.cpp
@@ -1,50 +1,72 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <string.h>
+
+#ifdef WIN32
+    #define LIMPA_TELA system("cls")
+#else
+    #define LIMPA_TELA system("clear")
+#endif
 
 //Caracterização de um JOB
-typedef struct registro{
+typedef struct Jobs{
+  char descricaoJob[25];
   int duracao;
   int deadline;
-  struct registro *prox;
-}registro;
+  struct Jobs *prox;
+}Jobs;
 
-typedef struct listaJobs{
+typedef struct ListaJobs{
   int quantidade;
-  registro *inicio;
-}
+  Jobs *inicio;
+}ListaJobs;
 
 //Declaração de funções
-void sortJobs(registro *reg);
-void minimizeLateness(listaJobs *reg);
-void imprimeJobs(registro *reg);
-void alocaJob(registro *reg);
+void recebeDescricao(char *);
+int recebeValor(char *);
+void insereJobs(Jobs *, char *, int, int);
+void imprimeJobs(Jobs *);
+void alocaJob(Jobs *);
 int menu();
 
 int main(){
-  int lateness, opcao;
-  registro *jobs;
+  int lateness, opcao, duracao, deadline;
+  Jobs *jobs;
+  char descricaoJob[25];
+  char stringDuracao[8] = "duracao";
+  char stringDeadline[9] = "deadline";
 
-  opcao = menu();
-
-  switch (opcao) {
-    case 1:
-      sortJobs(jobs);
-      minimizeLateness(jobs);
-      printf("\nEventos estão sendo escalados\n");
-    break;
-    case 2:
-      printf("\nInserção de Elemento\n");
-    break;
-    case 3:
-      printf("\n ------------- Lista de Jobs -------------\n\n");
-      imprimeJobs(jobs);
-    break;
-    case 0:
-    break;
-    default:
-      printf("\nOpcao nao encontrada!!!!\n");
-  }
-
+  do{
+    opcao = menu();
+    switch (opcao) {
+      case 1:
+        printf("\nEventos estão sendo escalados\n");
+      break;
+      case 2:
+        recebeDescricao(descricaoJob);
+        duracao = recebeValor(stringDuracao);
+        deadline = recebeValor(stringDeadline);
+        insereJobs(jobs, descricaoJob, duracao, deadline);
+        // printf("%s\n", descricaoJob);
+        // printf("%d\n", duracao);
+        // printf("%d\n", deadline);
+        // printf("\nInserção de Elemento\n");
+      break;
+      case 3:
+        printf("\n ------------- Lista de Jobs -------------\n\n");
+        imprimeJobs(jobs);
+      break;
+      case 0:
+        LIMPA_TELA;
+        printf("Programa finalizado!\n");
+        exit(0);
+        break;
+      default:
+        printf("\nOpcao nao encontrada!!!!\n");
+        break;
+    }
+  }while(opcao != 0);
+  return 0;
 }
 
 int menu(){
@@ -58,26 +80,55 @@ int menu(){
   return op;
 }
 
-//FUNÇÕES
+//=============== SUBPROGRAMAS ==============
 
-void sortJobs(registro *reg){
-  //Ordenar Jobs por deadline (Crescente)
+void recebeDescricao(char *descricaoJob){
+   LIMPA_TELA;
+   printf("Digite a descricao do Job que deseja inserir: ");
+   scanf("%s", descricaoJob);
 }
 
-void minimizeLateness(listaJobs *reg){
-  //Escalar Jobs
-  int tempoInicial = 0;
-  registro *atual = reg->inicio;
-
-  /*for(int j = 0; j<= reg->quantidade; j++){
-    alocaJob(atual, tempoInicial , tempoInicial + atual->duracao);
-    atual->duracao = tempoInicial;
-    tempoFinal = tempoInicial + duracao;
-    tempoInicial = tempoInicial + duracao;
-  }
-  */  
+int recebeValor(char* tipo){
+  int valor;
+  LIMPA_TELA;
+  printf("Digite o(a) %s do Job que deseja inserir: ", tipo);
+  scanf("%d", &valor);
+  return valor;
 }
 
-void imprimeJobs(registro *reg){
+void insereJobs(Jobs *jobs , char *descricaoJob, int duracao, int deadline){
+    Jobs *atual, *novo, *anterior;
+    int num;
+
+    novo = (Jobs *) malloc(sizeof(Jobs));
+
+    atual = jobs;
+    anterior = NULL;
+
+    memset(novo->descricaoJob, '\0', sizeof(novo->descricaoJob));
+    strcpy(novo->descricaoJob, descricaoJob);
+    novo->duracao = duracao;
+    novo->deadline = deadline;
+
+    if(atual == NULL){
+        novo->prox = NULL;
+        jobs = novo;
+    } else{
+        while(atual != NULL && atual->deadline < num){
+            anterior = atual;
+            atual = atual->prox;
+        }
+
+        novo->prox = atual;
+
+        if(anterior == NULL){
+            jobs = novo;
+        } else{
+            anterior->prox = novo;
+        }
+    }
+}
+
+void imprimeJobs(Jobs *reg){
   //Imprime lista de Jobs
 }
